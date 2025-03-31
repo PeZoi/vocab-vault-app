bdc2397c853264f212b0f35bef6bd01c16e8941c
PeZoi <pezoiks1@gmail.com>
2025-03-31 14:34:42 +0700

    hotfix: merge

---
+++ vocab_vault_fe/src/apis/flashCardAPI.ts
@@ -0,0 +1,7 @@
+import { ApiResponse } from "types";
+import axiosInstance from "utils/axiosInterceptor";
+
+export const getFlashCardAPI = async (id: any): Promise<ApiResponse> => {
+  const { data } = await axiosInstance.get(`/api/flash-cards/get/${id}`);
+  return data;
+}
